907a924feca088ca8116b443d5c61b85c8d1ac21
Sandip <sandipdhakal487@gmail.com>
2024-10-02 22:07:07 +0545

    database connections setup

--- src/server.js
+++ src/server.js
@@ -1,46 +1,34 @@
-// const express = require("express"); // common js, which works synchronously or uses synchronous loading
-import express from "express"; // ES module js, which works asynchronously or uses asynchronous loading
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+/*
+import express from "express";
 const app = express();
 
-// Get a list of five jokes
-
-app.get("/api/jokes", (req, res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: "Skeleton Fight",
-      content:
-        "Why don't skeletons fight each other? They don't have the guts!",
-    },
-    {
-      id: 2,
-      title: "Scarecrow Award",
-      content:
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-    },
-    {
-      id: 3,
-      title: "Talking Walls",
-      content:
-        "What did one wall say to the other wall? 'I'll meet you at the corner!'",
-    },
-    {
-      id: 4,
-      title: "Sad Math Book",
-      content:
-        "Why did the math book look sad? Because it had too many problems.",
-    },
-    {
-      id: 5,
-      title: "Fake Spaghetti",
-      content: "What do you call fake spaghetti? An 'impasta'!",
-    },
-  ];
-  res.send(jokes);
-});
-
 const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening at port ${process.env.PORT} `);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
 
 app.listen(port, () => {
   console.log(`Serve at https://localhost:${port}`);
 });
+*/
